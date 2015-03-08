0fa99812ffac9a4b6451f4a0975cbb671775ebda
Taylor Kisor-Smith <tfkisorsmith@gmail.com>
2015-03-07 21:52:15 -0800

    Copy.

--- src/util.js
+++ src/util.js
@@ -13,6 +13,12 @@ var util = require('util'),
 util._ = _;
 util.url = url;
 
+
+util.copy = function (simpleObj) {
+    // why? because FUCK YOU! that's why!
+    return JSON.parse(JSON.stringify(simpleObj));
+};
+
 util.listFiles = function (dir) {
 
     var results = [];
