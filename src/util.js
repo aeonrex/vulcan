0e80bf686433a2cfa77b82b449376a8c66ec1e48
Taylor Kisor-Smith <tfkisorsmith@gmail.com>
2015-03-07 19:43:16 -0800

    Inital commit of working tool box.

--- src/util.js
+++ src/util.js
@@ -1,3 +1,15 @@
 /**
  * Created by taylorks on 3/7/15.
  */
+
+'use strict';
+
+var util = require('util'),
+    _ = require('lodash'),
+    url = require('url');
+
+
+util._ = _;
+util.url = url;
+
+module.exports = util;
--- src/vulcan.js
+++ src/vulcan.js
@@ -1,3 +1,15 @@
 /**
  * Created by taylorks on 3/7/15.
  */
+'use strict';
+
+var nconf = require('nconf'),
+    util = require('./util'),
+    appConfigPath = util.url.resolve(process.cwd(), '/config');
+
+module.exports = function () {
+    nconf
+        .env()
+        .argv()
+        .file('app', appConfigPath + '.json');
+}();
